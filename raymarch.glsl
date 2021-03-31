abf60a36ae0e62d7ec08eb174e2143a429797c24
Ryan Wang <rw07041+Github@gmail.com>
2021-03-30 22:01:07 -0400

    add phong lighting

--- raymarch.glsl
+++ raymarch.glsl
@@ -49,6 +49,15 @@ float SDF(vec3 pos)
     return sphereSDF(pos, vec3(0.0), 1.0);
 }
 
+vec3 normal(vec3 pos)
+{
+    const vec2 STEP = vec2(0.0001, 0.0);
+    float gx = SDF(pos + STEP.xyy) - SDF(pos - STEP.xyy);
+    float gy = SDF(pos + STEP.yxy) - SDF(pos - STEP.yxy);
+    float gz = SDF(pos + STEP.yyx) - SDF(pos - STEP.yyx);
+    return normalize(vec3(gx, gy, gz));
+}
+
 // cast ray and return the distance from the origin to the scene
 float castRay(vec3 rayOrigin, vec3 rayDir)
 {
@@ -75,17 +84,45 @@ float castRay(vec3 rayOrigin, vec3 rayDir)
     return -1.0;
 }
 
+// calculate phong lighting, adapted from https://www.tomdalling.com/blog/modern-opengl/07-more-lighting-ambient-specular-attenuation-gamma/
+vec3 phong(vec3 surfacePos, vec3 camPos, light l, material m)
+{
+    vec3 n = normal(surfacePos);
+    vec3 surfaceToLight = normalize(l.pos - surfacePos);
+    vec3 surfaceToCam = normalize(camPos - surfacePos);
+    vec3 reflection = reflect(-surfaceToLight, n);
+    float distToLight = length(l.pos - surfacePos);
+
+    vec3 ambient = l.ambient * m.color * l.intensity;
+
+    float diffuseCoefficient = max(0.0, dot(n, surfaceToLight));
+    vec3 diffuse = diffuseCoefficient * m.color * l.intensity;
+
+    float specularCoefficient = 0.0;
+    if (diffuseCoefficient > 0.0)
+    {
+        specularCoefficient = pow(max(0.0, dot(surfaceToCam, reflection)), m.shininess);
+    }
+    vec3 specular = specularCoefficient * m.specular * l.intensity;
+
+    float attenuation = 1.0 / (1.0 + l.attenuation * pow(distToLight, 2.0));
+    return ambient + attenuation * (diffuse + specular);
+}
+
 // render scene
 vec3 render(vec3 rayOrigin, vec3 rayDir)
 {
-    float pos = castRay(rayOrigin, rayDir);
-    if (pos < 0.0)
+    const vec3 gamma = vec3(1.0 / 2.2);
+    light l = light(vec3(1.0, 1.0, 1.0), vec3(1.0), 1.0, 0.05);
+    material m = material(vec3(0.3, 0.8, 0.2), vec3(1.0), 10.0);
+    float depth = castRay(rayOrigin, rayDir);
+    if (depth < 0.0)
     {
         // skybox
         return vec3(0.3, 0.3, 0.6) - (rayDir.y * 0.25);
     }
-    // simple shading
-    return vec3(1.0 - pos * 0.5);
+    vec3 color = phong(rayOrigin + rayDir * depth, rayOrigin, l, m);
+    return pow(color, gamma);
 }
 
 void main()
